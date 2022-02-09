7fd5110ac1d6a6f27856b0bfb6c8217172d798c6
Ard Scheirlynck <ard_scheirlynck@msn.com>
2022-02-09 13:24:04 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-function hello (persoon: string) {
-    basic.showString("Hallo" + persoon)
+namespace ardMagic{
+    /**
+     * Dummy comments
+     */
+    //% blockId=ardMagicHello
+    //% block="hello"
+    export function hello(persoon: string) {
+        basic.showString("Hallo" + persoon)
+    }
+
 }
